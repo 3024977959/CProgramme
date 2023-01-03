6f33f7b3dfb449d57b3c0e84bcececc9604d6eaf
浮世壹梦 <108175071+3024977959@users.noreply.github.com>
2023-01-03 23:24:20 +0800

    Add files via upload

---
+++ 加法运算.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("输入数字进行加法操作");
+            int a = Convert.ToInt32(Console.ReadLine());//把输入的字符转换为int类型的整数
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
